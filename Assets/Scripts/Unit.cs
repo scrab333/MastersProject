fdc6db9bcbe3090d1d451b300e439761bc87637f
GooseGoesHjonk <REESdanielk@aol.com>
2023-10-20 13:39:22 +0100

    Unit Move
    Got unit to move

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        float stoppingDistance = .1f;
+        if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance) //So we stop at the position
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized; //Find move direction
+            float moveSpeed = 4f; 
+            transform.position += moveDirection * moveSpeed * Time.deltaTime; //Move unit, framerate independant
+        }
+       
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4)); //Moves to position 
+        }    
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition; //Set targetPosition
+    }
+}
