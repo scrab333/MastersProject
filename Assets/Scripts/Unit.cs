91c6dcf8a54e5b38f5279853ba9af961706bfead
scrab333 <72270337+scrab333@users.noreply.github.com>
2023-10-30 01:42:33 +0000

    Level grid
    Basically refactored most of the grid system and made it to display position of a unit.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -10,10 +10,17 @@ public class Unit : MonoBehaviour
     [SerializeField] private Animator unitAnimator;
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
     private void Update()
@@ -34,7 +41,15 @@ public class Unit : MonoBehaviour
         {
             unitAnimator.SetBool("Is_Running", false);
         }
-       
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if(newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMoveGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+
+
 
         //if (Input.GetMouseButtonDown(0))
         //{
