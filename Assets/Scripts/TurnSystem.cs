2941f4a1d81e8cbbaf3de53af13e962d5cfd794f
scrab333 <72270337+scrab333@users.noreply.github.com>
2023-11-07 22:55:42 +0000

    Turns system
    Well now you have turns in the game

---
+++ Assets/Scripts/TurnSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TurnSystem : MonoBehaviour
+{
+    public static TurnSystem Instance { get; private set; }
+
+    public event EventHandler OnTurnChanged;
+
+    private int turnNumber = 1;
+
+    private void Awake() //Set on awake instance of the object
+    {
+        if (Instance != null) //Error Checking if there is only one instance of the script
+        {
+            Debug.LogError("There is more tha one TurnSystem" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void NextTurn()
+    {
+        turnNumber++;
+        OnTurnChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public int GetTurnNumber()
+    {
+        return turnNumber;
+    }
+}
