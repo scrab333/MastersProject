e32f9be38c51b2c3ff24efda87c15cfe760000a9
Bartosz Swieszkowski <30021341@students.southwales.ac.uk>
2023-11-26 22:43:15 +0000

    Enemy AI structure start

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy
+    }
 
+    private State state;
     private float timer;
 
     private void Start()
     {
         TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
+    }
+
+    private void Awake()
+    {
+        state = State.WaitingForEnemyTurn;
     }
 
     private void Update()
@@ -20,17 +32,31 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        timer -= Time.deltaTime;
-        if (timer <= 0f)
+        switch (state)
         {
-            TurnSystem.Instance.NextTurn();
-        }
+            case State.WaitingForEnemyTurn:
+                break;
+            case State.TakingTurn:
+                timer -= Time.deltaTime;
+                if (timer <= 0f)
+                {
+                    state = State.WaitingForEnemyTurn;
+                    TurnSystem.Instance.NextTurn();
+                }
+                break;
+            case State.Busy:
+                break;
+        }    
     }
 
 
     private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
     {
-        timer = 2f;
+        if (!TurnSystem.Instance.IsPlayerTurn())
+        {
+            state = State.TakingTurn;
+            timer = 2f;
+        }
     }
 
 }
