514b05aa22dca630ea234ca994d83a3db5761819
scrab333 <72270337+scrab333@users.noreply.github.com>
2024-01-18 14:25:27 +0000

    Cleaning up the code
    By the use of generic i implemented a function that just checks for an action once, where before we had to define them seperatly

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -20,18 +20,12 @@ public class Unit : MonoBehaviour
 
     private GridPosition gridPosition;
     private HealthSystem healthSystem;
-    private MoveAction moveAction;
-    private SpinAction spinAction;
-    private ShootAction shootAction;
     private BaseAction[] baseActionArray;
     private int actionPoints = ACTION_POINTS_MAX;
 
     private void Awake()
     {
         healthSystem = GetComponent<HealthSystem>();
-        moveAction = GetComponent<MoveAction>();
-        spinAction = GetComponent<SpinAction>();
-        shootAction = GetComponent<ShootAction>();
         baseActionArray = GetComponents<BaseAction>();
     }
 
@@ -65,20 +59,18 @@ public class Unit : MonoBehaviour
         //}    
     }
 
-    public MoveAction GetMoveAction()
+    public T GetAction<T>() where T : BaseAction
     {
-        return moveAction;
-    }
-    
-    public SpinAction GetSpinAction()
-    {
-        return spinAction;
+        foreach (BaseAction baseAction in baseActionArray)
+        {
+            if(baseAction is T)
+            {
+                return (T)baseAction;
+            }
+        }
+        return null;
     }
 
-    public ShootAction GetShootAction()
-    {
-        return shootAction;
-    }
 
     public GridPosition GetGridPosition()
     {
