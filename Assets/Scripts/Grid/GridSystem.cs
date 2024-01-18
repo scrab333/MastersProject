a300030b7ccdc4d919a2f532a2179664670be939
scrab333 <72270337+scrab333@users.noreply.github.com>
2024-01-18 14:48:31 +0000

    Grid system refactor
    Changed the grid system a bit to fit the pathfinding script

--- Assets/Scripts/Grid/GridSystem.cs
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -1,30 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Security.Cryptography;
 using UnityEngine;
 
-public class GridSystem
+public class GridSystem<TGridObject> 
 {
     private int width;
     private int height;
     private float cellSize;
-    private GridObject[,] gridObjectArray;
+    private TGridObject[,] gridObjectArray;
 
-    public GridSystem(int width, int height, float cellSize)
+    public GridSystem(int width, int height, float cellSize, Func<GridSystem<TGridObject>,GridPosition, TGridObject> createGridObject)
     {
         this.width = width;
         this.height = height; //Sets the width and height of the grid
         this.cellSize = cellSize; //Initialises cell size
 
-        gridObjectArray = new GridObject[width, height];
+        gridObjectArray = new TGridObject[width, height];
         
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++) //Draw the grid
             {
                 GridPosition gridPosition = new GridPosition(x, z);
-                gridObjectArray[x,z] = new GridObject(this, gridPosition);
+                gridObjectArray[x,z] = createGridObject(this, gridPosition);
             }
         }
     }
@@ -53,12 +54,12 @@ public class GridSystem
 
                 Transform debugTransform = GameObject.Instantiate(debugPrefab, GetWorldPosition(gridPosition), Quaternion.identity); //For grid debug testing
                 GridDebugObject gridDebugObject = debugTransform.GetComponent<GridDebugObject>();
-                gridDebugObject.SetGridObject(GetGridObject(gridPosition));
+                gridDebugObject.SetGridObject(GetGridObject(gridPosition) as GridObject);
             }
         }
     }
 
-    public GridObject GetGridObject(GridPosition gridPosition)
+    public TGridObject GetGridObject(GridPosition gridPosition)
     {
         return gridObjectArray[gridPosition.x, gridPosition.z];
     }
--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -10,7 +10,7 @@ public class LevelGrid : MonoBehaviour
     public event EventHandler OnAnyUnitMovedGridPosition;
 
     [SerializeField] private Transform gridDebugObjectPrefab;
-    private GridSystem gridSystem;
+    private GridSystem<GridObject> gridSystem;
 
     private void Awake()
     {
@@ -24,7 +24,7 @@ public class LevelGrid : MonoBehaviour
 
 
 
-        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem = new GridSystem<GridObject>(10, 10, 2f, (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
 
