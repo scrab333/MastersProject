a70167b1dbc6ded0f64eb497a21e32b8375e9496
GooseGoesHjonk <REESdanielk@aol.com>
2024-01-20 17:24:36 +0000

    Some changes
    Made some additions

--- Assets/Scripts/Grid/GridSystemVisual.cs
+++ Assets/Scripts/Grid/GridSystemVisual.cs
@@ -100,6 +100,29 @@ public class GridSystemVisual : MonoBehaviour
 
         ShowGridPositionList(gridPositionList, gridVisualType);
     }
+    
+    private void ShowGridPositionRangeSquare(GridPosition gridPosition, int range, GridVisualType gridVisualType)
+    {
+        List<GridPosition> gridPositionList = new List<GridPosition>();
+
+        for(int x = -range ; x <= range; x++)
+        {
+            for (int z = -range ; z <= range ; z++)
+            {
+                GridPosition testGridPosition = gridPosition + new GridPosition(x, z);
+
+                if(!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    continue;
+                }
+
+
+                gridPositionList.Add(testGridPosition);
+            }
+        }
+
+        ShowGridPositionList(gridPositionList, gridVisualType);
+    }
 
     public void ShowGridPositionList(List<GridPosition> gridPositionList, GridVisualType gridVisualType)
     {
@@ -132,6 +155,14 @@ public class GridSystemVisual : MonoBehaviour
                 gridVisualType = GridVisualType.Red;
 
                 ShowGridPositionRange(selectedUnit.GetGridPosition(), shootAction.GetMaxShootDistance(), GridVisualType.RedSoft);
+                break;
+            case GrenadeAction grenadeAction:
+                gridVisualType = GridVisualType.Yellow;
+                break;
+            case SwordAction swordAction:
+                gridVisualType = GridVisualType.Red;
+
+                ShowGridPositionRangeSquare(selectedUnit.GetGridPosition(), swordAction.GetMaxSwordDistance(), GridVisualType.RedSoft);
                 break;
         }    
 
