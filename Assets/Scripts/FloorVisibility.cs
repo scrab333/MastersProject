340c10ce8e659a17cda3eddee196561b1ddb4549
Bartosz Swieszkowski <30021341@students.southwales.ac.uk>
2024-02-04 18:39:55 +0000

    Multifloor - Visibility with Camera angles -Basic approach

---
+++ Assets/Scripts/FloorVisibility.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorVisibility : MonoBehaviour
+{
+    private Renderer[] rendererArray;
+    private int floor;
+
+    private void Awake()
+    {
+        rendererArray = GetComponentsInChildren<Renderer>(true);
+    }
+
+    private void Start()
+    {
+        floor = LevelGrid.Instance.GetFloor(transform.position);
+
+        if(floor == 0)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    { 
+        float cameraHeight = CameraController.Instance.GetCameraHeight();
+
+        float floorHeightOffset = 2f;
+        bool showObject = cameraHeight > LevelGrid.FLOOR_HEIGHT * floor + floorHeightOffset;
+
+        if (showObject)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        foreach (Renderer renderer in rendererArray)
+        {
+            renderer.enabled = true;
+        }
+    }
+
+    private void Hide()
+    {
+        foreach (Renderer renderer in rendererArray)
+        {
+            renderer.enabled = false;
+        }
+    }
+}
