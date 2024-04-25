9acf16fbfe5ea36d93712094a6f88b89dfdcb40c
Bartosz Swieszkowski <30021341@students.southwales.ac.uk>
2024-04-26 00:08:31 +0100

    Main Menu Finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject OptionPanel;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,8 +17,13 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void Settings()
+    public void OptionPanelOpen()
     {
+        OptionPanel.SetActive(true);
+    }
 
+    public void OptionPanelClose()
+    {
+        OptionPanel.SetActive(false);
     }
 }
